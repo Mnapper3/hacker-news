df36f14581f6b4e199ef8c3d8c0cdc12fef80c04
nappdaddy2000 <mnapper3@ivytech.edu>
2017-01-09 03:37:25 +0000

    comments edit and delete btn

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,40 @@
 class MessagesController < ApplicationController
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
     
   def index
     @messages = Message.all.order("created_at DESC")
   end
 
 	def show
-	  @message = Message.find(params[:id])
 	end
 
 	def new
-		@message = Message.new
+		@message = current_user.messages.build
 	end
 
 	def create
-		@message = Message.new(message_params)
+		@message = current_user.messages.build(message_params)
 		if @message.save
 			redirect_to root_path
 		else
 			render 'new'
 		end
+	end
+	
+	def edit
+	end
+	
+	def update
+	  if @message.update(message_params)
+	    redirect_to message_path
+	   else
+	     render 'edit'
+	  end
+	end
+	
+	def destroy
+	  @message.destroy
+	  redirect_to root_path
 	end
   
   private
@@ -27,4 +43,7 @@ class MessagesController < ApplicationController
 		params.require(:message).permit(:title, :description)
 	end
 	
+	def find_message
+	  @message = Message.find(params[:id])
+	end
 end
